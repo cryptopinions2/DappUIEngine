28d5537228aa27547ca349d9e73520032082b27a
cryptopinions <cryptopinions@email.com>
2018-10-16 19:56:45 -0600

    lots of stuff, mostly more backgrounds, more coherent randomization

--- uiEngine.js
+++ uiEngine.js
@@ -23,21 +23,85 @@ var uiEngine={
 
     document.getElementsByTagName('head')[0].appendChild(style);
   },
+  getRandomSeed:function(rng){
+    return rng.random().toString(36).substring(7);
+  },
+  getNewRandom:function(seed){
+    var rng
+    if(seed){
+      rng=new Math.seedrandom(seed)
+    }
+    else{
+      rng=new Math.seedrandom()
+    }
+    rng.random=rng.quick
+    rng.getRandomInt=function(maxint){
+      return Math.floor(rng.random() * maxint)
+    }
+    return rng
+  },
+  displaySpecification:{
+    schemeHue:'random',
+    schemeColorVariation:'random',
+    bgMethod:'random',
+    bg0seed:'random',
+    bg1seed:'random',
+  },
+  randChoice:function(alist,rng){
+    return alist[Math.floor(rng.random()*alist.length)]
+  },
+  fillSpecifications:function(seed){
+    var newspec = JSON.parse(JSON.stringify(uiEngine.displaySpecification));
+    function isr(athing){
+      return athing=='random'
+    }
+    var rng=uiEngine.getNewRandom(seed)
+    var localrng;
+    var localseed;
+    if(isr(newspec.schemeHue)){
+      newspec.schemeHue=Math.floor(140+rng.random() * (340-140))
+    }
+    if(isr(newspec.schemeColorVariation)){
+      newspec.schemeColorVariation='default'
+    }
+    if(isr(newspec.schemeColorVariation)){
+      newspec.schemeColorVariation=uiEngine.randChoice(['default','pastel','soft','light','hard','pale'],rng)
+    }
+    if(isr(newspec.bgMethod)){
+      newspec.bgMethod=Math.floor(rng.random()*4)
+    }
+    if(isr(newspec.bg0seed)){
+      newspec.bg0seed=uiEngine.getRandomSeed(rng)
+    }
+    if(isr(newspec.bg1seed)){
+      newspec.bg1seed=uiEngine.getRandomSeed(rng)
+    }
+    if(isr(newspec.bg2seed)){
+      newspec.bg2seed=uiEngine.getRandomSeed(rng)
+    }
+    if(isr(newspec.bg3seed)){
+      newspec.bg3seed=uiEngine.getRandomSeed(rng)
+    }
+    return newspec
+  },
   transformSite:function(seed){
     // Use "new" to create a local prng without altering Math.random.
     //var myrng = new Math.seedrandom('hello.');
     //Math.seedrandom();
     //var randomseed=Math.random().toString(36).substring(7);
     //console.log('seed is: ',randomseed)
-    var rng=new Math.seedrandom('g2jkv')//randomseed)
-    rng.random=rng.quick
+    var rng;
+    rng=uiEngine.getNewRandom(seed)//randomseed)
+    var displaySpec=uiEngine.fillSpecifications(seed)
+
+
     var scheme = new ColorScheme;
     scheme.add_complement(true)
-    scheme.from_hue(Math.floor(rng.random() * 360))//getRandomInt(0,360)-1)         // Start the scheme
+    scheme.from_hue(displaySpec.schemeHue)//Math.floor(140+rng.random() * (340-140)))//getRandomInt(0,360)-1)         // Start the scheme
       .scheme('analogic')     // Use the 'triade' scheme, that is, colors
                             // selected from 3 points equidistant around
                             // the color wheel.
-      .variation('default');   // Use the 'soft' color variation
+      .variation(displaySpec.schemeColorVariation)//'default');   // Use the 'soft' color variation
     var colors = scheme.colors();
     document.body.style.background='#'+colors[1]
     uiEngine.setStyle('a:hover,a:focus{color: #'+colors[11]+';}')
@@ -64,16 +128,56 @@ var uiEngine={
     canvas.width=1000
     canvas.height=500
     var colorsCopy=colors.slice();
-    var opts = {
-            ShowDebug: false,
-            MonochromaticOnly: true,
-            colorsToUse: colorsCopy.slice(0,4).splice(0,2),
-            random:rng,
-        };
-    console.log('colorsafter',colors)
-    GenerateMatBG(canvas,opts)
-    console.log('colorsafter2',colors)
-    document.body.style.background = "url(" + canvas.toDataURL() + ") ";
+
+    var bgMethod=displaySpec.bgMethod//Math.floor(rng.random()*3);
+    var dataUrl;
+    if(bgMethod==0){
+      var opts = {
+              ShowDebug: false,
+              MonochromaticOnly: true,
+              colorsToUse: colorsCopy.slice(0,4).splice(0,2),
+              random:uiEngine.getNewRandom(displaySpec.bg0seed),
+          };
+      console.log('colorsafter',colors)
+      GenerateMatBG(canvas,opts)
+      console.log('colorsafter2',colors)
+      dataUrl="url(" +canvas.toDataURL() + ") ";
+    }
+    if(bgMethod==1){
+      var colors2=colorsCopy.slice(0,4).splice(0,2)
+      console.log('colors2',colors2)
+      var localrng=uiEngine.getNewRandom(displaySpec.bg1seed)
+      var pattern = Trianglify({
+        height: window.innerHeight,
+        width: window.innerWidth,
+        cell_size: 30 + localrng.random() * 100,
+        seed:displaySpec.bg1seed,//uiEngine.getRandomSeed(rng),
+        x_colors:uiEngine.randChoice([['#FFFFFF', colorsCopy[0], '#000000'],['#000000', colorsCopy[0], '#FFFFFF'],['#D8D8D8', colorsCopy[0], '#424242']],localrng),
+        y_colors:uiEngine.randChoice([false,['#FFFFFF', colorsCopy[0], '#000000'],['#000000', colorsCopy[0], '#FFFFFF'],['#D8D8D8', colorsCopy[0], '#424242']],localrng)});//false})//['#FFFFFF', colorsCopy[0], '#000000']['#000000', colorsCopy[0], '#FFFFFF']
+      canvas=pattern.canvas();
+      dataUrl="url(" +canvas.toDataURL() + ") ";
+    }
+    if(bgMethod==2){
+      var pattern = GeoPattern.generate(displaySpec.bg2seed,{'color':colors[0]});
+      dataUrl=pattern.toDataUrl()//pattern.toDataURL()
+      //console.log('geopattern output '+pattern.toDataUrl());
+    }
+    if(bgMethod==3){
+      var localseed=displaySpec.bg3seed//uiEngine.getRandomSeed(rng)
+      console.log('bgmethod 3 local seed ',localseed)
+      localrng=uiEngine.getNewRandom(localseed)
+      //console.log('localrngright? ',localrng.getRandomInt(360))
+      var generator = new ColorfulBackgroundGenerator();
+      generator.addLayer(new ColorfulBackgroundLayer({degree: localrng.getRandomInt(360), h: localrng.getRandomInt(360), s: 0.95, l: 0.55,posColor: 100})); // bottom layer
+      generator.addLayer(new ColorfulBackgroundLayer({degree: localrng.getRandomInt(360), h: localrng.getRandomInt(360), s: 0.9, l: 0.7, posColor: 30, posTransparency: 80}));
+      generator.addLayer(new ColorfulBackgroundLayer({degree: localrng.getRandomInt(360), h:localrng.getRandomInt(360), s: 0.95, l: 0.7, posColor: 10, posTransparency: 80}));
+      generator.assignStyleToElement(document.body);
+      dataUrl=false
+    }
+    //console.log('canvas is ',canvas)
+    if(dataUrl){
+      document.body.style.background = dataUrl ;
+    }
     document.body.style['background-size']="1500px 900px"
   }
 }
